8d62670a20fce7dfc5ba0db56dc59e17953edb03
GwerhDev <gwerh.dev@gmail.com>
2025-02-12 15:09:18 -0300

    changing names for NHEXA

--- src/middlewares/misc/const.ts
+++ src/middlewares/misc/const.ts
@@ -1,3 +1,3 @@
 import { environment } from "../../environment";
 
-export const API_URL: string = environment === "development" ? 'http://localhost:3001' : 'https://laruinahub-api.fly.dev';
+export const API_URL: string = environment === "development" ? 'http://localhost:8080' : 'https://nhexa-api.fly.dev';

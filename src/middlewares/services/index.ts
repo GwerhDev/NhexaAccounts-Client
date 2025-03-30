4eb9a86e8fab4a63abb56f1d466b276154ce4dd2
GwerhDev <gwerh.dev@gmail.com>
2025-03-29 23:59:12 -0300

    no tokens in headers

--- src/middlewares/services/index.ts
+++ src/middlewares/services/index.ts
@@ -1,31 +1,31 @@
-import axios from "axios"
-import { API_URL } from "../misc/const"
+import axios from "axios";
+import { API_URL } from "../misc/const";
 import { error } from "../misc/errors";
 
 export const signupGoogle: any = async (formData: any) => {
   const response: any = await axios.post(API_URL + "/signup-google", formData)
-    .then(response => response.data.token)
+    .then(response => response.data)
     .catch(() => { return { error: error.api.loadAllItems } });
   return response;
 };
 
 export const loginGoogle: any = async (formData: any) => {
   const response: any = await axios.post(API_URL + "/login-google", formData)
-    .then(response => response.data.token)
+    .then(response => response.data)
     .catch(() => { return { error: error.api.loadAllItems } });
   return response;
 };
 
 export const signupInner: any = async (formData: any) => {
   const response: any = await axios.post(API_URL + "/signup-inner", formData)
-    .then(response => response.data.token)
+    .then(response => response.data)
     .catch(() => { return { error: error.api.loadItemById } });
   return response;
 };
 
 export const loginInner: any = async (formData: any) => {
   const response: any = await axios.post(API_URL + "/login-inner", formData)
-    .then(response => response.data.token)
+    .then(response => response.data)
     .catch(() => { return { error: error.api.loadItemById } });
   return response;
 };

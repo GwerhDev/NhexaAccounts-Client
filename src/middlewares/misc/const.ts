b894d323a771789818bf32bc4f60329136920275
GwerhDev <gwerh.dev@gmail.com>
2025-04-15 12:33:45 -0400

    updating api url

--- src/middlewares/misc/const.ts
+++ src/middlewares/misc/const.ts
@@ -1,6 +1,6 @@
 import { environment } from "../../environment";
 
-export const API_URL: string = environment === "development" ? 'http://localhost:8080' : 'https://nhexa-api.fly.dev';
+export const API_URL: string = environment === "development" ? 'http://localhost:8080' : 'https://api.nhexa.cl';
 export const ACCOUNTS_URL: string = environment === "development" ? 'http://localhost:5173' : 'https://accounts.nhexa.cl';
 
 export const CLIENT_URL: string = environment === "development" ? 'http://localhost:5174' : 'https://nhexa.cl';

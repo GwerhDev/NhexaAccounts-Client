89660adaeb7afd2c4326b72ce5ba5c5650901e1b
gerardo <gerardo@recemed.com>
2025-10-23 16:01:19 -0300

    styling

--- src/app/components/Navigator/LateralButton.component.vue
+++ src/app/components/Navigator/LateralButton.component.vue
@@ -1,14 +1,16 @@
 <style scoped lang="scss" src="./LateralButton.component.scss" />
 <script setup lang="ts">
 import { openMenu, closeMenu } from '../../../helpers/menu';
+import closeIcon from '../../../assets/svg/close-icon.svg';
+import burgerIcon from '../../../assets/svg/icon-burger-menu.svg';
 
 </script>
 
 <template>
   <div class="burger-menu-container">
-    <img src="../../../assets/svg/icon-burger-menu.svg" @click="openMenu()" alt="">
+    <img :src="burgerIcon" @click="openMenu()" alt="">
   </div>
   <div class="close-menu-container" alt="">
-    <img src="../../../assets/svg/close-icon.svg" @click="closeMenu()" width="25" alt="">
+    <img :src="closeIcon" @click="closeMenu()" width="25" alt="">
   </div>
 </template>
